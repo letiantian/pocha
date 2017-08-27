a0b8edfd2d0387498df62a9f76371b4bfcd870e7
letian <sunlt1699@gmail.com>
2017-08-27 12:41:42 +0800

    really pocha

--- pocha/discover.py
+++ pocha/discover.py
@@ -78,42 +78,43 @@ def search(path, expression):
 
     # load each module and then we'll have a complete list of the tests
     # to run
+    def get_module_package_root_path_and_module_name(module_full_path):
+        module_full_path = module_full_path.replace('/', os.path.sep).replace('\\', os.path.sep)
+        path_list = module_full_path.split(os.path.sep)
+        if path_list[-1].endswith('.py'):
+            path_list[-1] = path_list[-1][:-3]
+        module_name_list = []
+        module_dir = os.path.sep.join(path_list[:-1])
+        while True:
+            if len(path_list) > 0:
+                module_name_list.insert(0, path_list.pop())
+                package_init_file_path = os.path.sep.join(path_list + ['__init__.py'])
+                if os.path.isfile(package_init_file_path):
+                    module_dir = os.path.sep.join(path_list[:-1])
+                else:
+                    break
+        # print '==: ', module_dir, module_name_list
+        return module_dir, '.'.join(module_name_list)
+
     for module in modules:
         cur_dir = os.getcwdu()
         module = module.replace('/', os.path.sep).replace('\\', os.path.sep)
         abs_path = os.path.abspath(os.path.join(cur_dir, module)).replace('/', os.path.sep).replace('\\', os.path.sep)
-        abs_dir = os.path.dirname(abs_path)
-        file_name = os.path.basename(abs_path)
-        module_name = file_name.replace('.py', '').replace('.pyc', '').replace('.pyo', '')
-        path_list = abs_path.split(os.path.sep)
-        if len(path_list) < 1 or not path_list[-1].endswith('.py') or len(path_list[-1]) < 4:
+        if not module.endswith('.py'):
             continue
-        path_list[-1] = path_list[-1][:-3]
-        # print '** ', path_list
-        for pos in xrange(len(path_list)-1, -1, -1):
-            
-            _path = os.path.sep.join(path_list[0:pos+1])
-            _real_module = '.'.join(path_list[pos+1:])
-            if _path == '' or _path is None:
-                _path = os.path.sep
-            if _real_module == '' or _real_module is None:
-                continue
-            print '## path: {}, module: {}'.format(_path, _real_module)
-            try:
-                if _real_module in sys.modules:
-                    del sys.modules[_real_module] 
-                sys.path.insert(0, _path)
-                __import__(_real_module)
-                break
-            except ImportError as e:
-                raise e
-            except Exception as e:
-                raise e
-            finally:
-                sys.path.remove(_path)
-        
-        # print '--module: ', module, os.getcwdu(), abs_dir, file_name, module_name, expression, path_list
-        # foo = imp.load_source('foo', module)
-    print common.TESTS
-    # print  '--lala: ', filter_tests(common.TESTS, expression)
+        # print get_module_package_root_path_and_module_name(abs_path)
+        module_dir, module_name = get_module_package_root_path_and_module_name(abs_path)
+        try:
+            if module in sys.modules:
+                del sys.modules[module] 
+            sys.path.insert(0, module_dir)
+            __import__(module_name)
+        except ImportError as e:
+            raise e
+        except Exception as e:
+            raise e
+        finally:
+            sys.path.remove(module_dir)
+
+    # print common.TESTS
     return filter_tests(common.TESTS, expression)
